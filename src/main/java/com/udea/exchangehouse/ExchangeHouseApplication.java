84f54f3b6e91fdb3f127169b4a864c0e9d3b8be0
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-08-17 15:53:18 -0500

    Hola Mundo!

--- src/main/java/com/udea/exchangehouse/ExchangeHouseApplication.java
+++ src/main/java/com/udea/exchangehouse/ExchangeHouseApplication.java
@@ -2,9 +2,18 @@ package com.udea.exchangehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ExchangeHouseApplication {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hola ciclo 3..... saldremos vivos de esto";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExchangeHouseApplication.class, args);
