9b365f2cc322fb2351ad19be7a6f9ad3fad2a3a9
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-08-17 14:59:53 -0500

    Initial commit

---
+++ src/main/java/com/udea/exchangehouse/ExchangeHouseApplication.java
@@ -0,0 +1,13 @@
+package com.udea.exchangehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeHouseApplication.class, args);
+    }
+
+}
