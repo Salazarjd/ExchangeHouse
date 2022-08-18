b203744b19cc6e098279fd48e90cc8421eb97653
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-08-18 06:34:16 -0500

    Entidad Empleado creada

--- src/main/java/com/udea/exchangehouse/models/Empleado.java
+++ src/main/java/com/udea/exchangehouse/models/Empleado.java
@@ -1,4 +1,68 @@
 package com.udea.exchangehouse.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Empleado")
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
+    private String rol;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String email, Empresa empresa, String rol) {
+        this.nombre = nombre;
+        this.email = email;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
